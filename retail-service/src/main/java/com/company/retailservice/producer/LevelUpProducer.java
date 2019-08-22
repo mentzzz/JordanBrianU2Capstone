ad5084b98b6fe2e3af6ea93ba4fcb572fefe0848
Jordan Mentz <mentzzz@me.com>
2019-08-22 15:16:43 -0400

    queue again

---
+++ retail-service/src/main/java/com/company/retailservice/producer/LevelUpProducer.java
@@ -0,0 +1,43 @@
+package com.company.retailservice.producer;
+
+import com.company.retailservice.dto.LevelUp;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+public class LevelUpProducer {
+    public static final String EXCHANGE = "level-up-exchange";
+    public static final String ROUTING_KEY = "level.#";
+    public static final String SAVE_QUEUED_MSG = "Point(s) queued for save";
+    public static final String UPDATE_QUEUED_MSG = "Point(s) queued for update";
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public LevelUpProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @RequestMapping(value = "/levelup", method = RequestMethod.POST)
+    public String createNote(@RequestBody @Valid LevelUp levelUp) {
+        LevelUp msg = new LevelUp(levelUp.getId(), levelUp.getCustomerId(), levelUp.getPoints(), levelUp.getMemberDate());
+        System.out.println("Sending points...");
+        rabbitTemplate.convertAndSend(EXCHANGE, ROUTING_KEY, msg);
+        System.out.println("Point(s) Sent");
+        return SAVE_QUEUED_MSG;
+    }
+
+    @PutMapping(value = "/notes/{id}")
+    public String updateNoteList(@RequestBody LevelUp levelUp) {
+
+        LevelUp msg = new LevelUp(levelUp.getId(), levelUp.getCustomerId(), levelUp.getPoints(), levelUp.getMemberDate());
+        System.out.println("Sending point(s)...");
+        rabbitTemplate.convertAndSend(EXCHANGE, ROUTING_KEY, msg);
+        System.out.println("Point(s) update sent");
+
+        return UPDATE_QUEUED_MSG;
+    }
+}
