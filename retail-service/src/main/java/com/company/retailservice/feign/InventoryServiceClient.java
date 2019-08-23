c82372ff442e1b69b777299d9442027fc8eef69a
bjsmith3185 <bjsmith3185@hotmail.com>
2019-08-23 09:58:02 -0400

    added routes to feign in retail service

--- retail-service/src/main/java/com/company/retailservice/feign/InventoryServiceClient.java
+++ retail-service/src/main/java/com/company/retailservice/feign/InventoryServiceClient.java
@@ -6,6 +6,7 @@ import com.company.retailservice.dto.InvoiceItem;
 import com.company.retailservice.dto.Product;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,15 +15,33 @@ import java.util.List;
 @FeignClient(name = "inventory-service")
 public interface InventoryServiceClient {
 
+    @RequestMapping(value = "/inventory", method = RequestMethod.POST)
+    public Inventory createInventory(@RequestBody Inventory inventory);
+
+    @RequestMapping(value = "/inventory", method = RequestMethod.GET)
+    public List<Inventory> getAllInventorys();
+
+    @RequestMapping(value = "/inventory/id/{id}", method = RequestMethod.GET)
+    public Inventory getInventoryById(@PathVariable int id);
+
+    @RequestMapping(value = "/inventory/id/{id}", method = RequestMethod.PUT)
+    public void updateInventory(@PathVariable int id, @RequestBody Inventory inventory);
+
+    @RequestMapping(value = "/inventory/{id}", method = RequestMethod.DELETE)
+    public void deleteInventory(@PathVariable int id);
 
 
-//    @RequestMapping(value = "/inventory/{inventory}", method = RequestMethod.GET)
-//    public int getQuantity(@PathVariable int inventory);
 
     @RequestMapping(value = "/inventory/product/{productid}", method = RequestMethod.GET)
     public Inventory getInventoryByProductId(@PathVariable int productid);
 
-    @RequestMapping(value = "/inventory/{inventoryId}", method = RequestMethod.GET)
-    public Inventory getInventoryById(@PathVariable int inventoryId);
+////    @RequestMapping(value = "/inventory/{inventory}", method = RequestMethod.GET)
+////    public int getQuantity(@PathVariable int inventory);
+//
+//    @RequestMapping(value = "/inventory/product/{productid}", method = RequestMethod.GET)
+//    public Inventory getInventoryByProductId(@PathVariable int productid);
+//
+//    @RequestMapping(value = "/inventory/{inventoryId}", method = RequestMethod.GET)
+//    public Inventory getInventoryById(@PathVariable int inventoryId);
 
 }
--- retail-service/src/main/java/com/company/retailservice/feign/InvoiceServiceClient.java
+++ retail-service/src/main/java/com/company/retailservice/feign/InvoiceServiceClient.java
@@ -3,9 +3,12 @@ package com.company.retailservice.feign;
 import com.company.retailservice.dto.Invoice;
 import com.company.retailservice.dto.InvoiceViewModel;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @FeignClient(name = "invoice-service")
 public interface InvoiceServiceClient {
@@ -13,12 +16,28 @@ public interface InvoiceServiceClient {
     @RequestMapping(value = "/invoices", method = RequestMethod.POST)
     public InvoiceViewModel createInvoice(@RequestBody InvoiceViewModel invoiceViewModel);
 
+    @RequestMapping(value = "/invoices/id/{id}", method = RequestMethod.GET)
+    public InvoiceViewModel getInvoiceById(@PathVariable int id);
 
-//    @RequestMapping(value = "/invoices/id/{id}", method = RequestMethod.GET)
-//    public InvoiceViewModel getInvoiceById(@PathVariable int id);
+    @RequestMapping(value = "/invoices", method = RequestMethod.GET)
+    public List<InvoiceViewModel> getAllInvoices();
 
-//    @RequestMapping(value = "/InvoiceItems", method = RequestMethod.GET)
-//    public List<InvoiceItem> getAllInvoiceItemss();
+    @RequestMapping(value = "/invoices/id/{id}", method = RequestMethod.PUT)
+    public void updateInvoice(@PathVariable int id, @RequestBody InvoiceViewModel invoiceViewModel);
+
+    @RequestMapping(value = "/invoices/id/{id}", method = RequestMethod.DELETE)
+    public void deleteInvoice(@PathVariable int id);
+
+
+//    @RequestMapping(value = "/invoices", method = RequestMethod.POST)
+//    public InvoiceViewModel createInvoice(@RequestBody InvoiceViewModel invoiceViewModel);
+//
+//
+////    @RequestMapping(value = "/invoices/id/{id}", method = RequestMethod.GET)
+////    public InvoiceViewModel getInvoiceById(@PathVariable int id);
+//
+////    @RequestMapping(value = "/InvoiceItems", method = RequestMethod.GET)
+////    public List<InvoiceItem> getAllInvoiceItemss();
 
 
 }
